32a4917742beb27e741cdb7bc115c542ffc1428f
Brahmantya Wardhana <bwardhana@hawk.iit.edu>
2023-11-06 13:18:51 -0600

    added css folder with reset, js file, and html for the 2018 congress page

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// jQuery 3.x-style ready event and locally scoped $
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
+
+// function for username  digit requirement
+
+var form = document.getElementById('form');
+
+// alert for form completion
+function alertFunction() {
+  if (form.checkValidity()) {
+    alert("You have successfully signed up\nCheck your email for more information");
+  }
+}
