cfbd3b8bf68baf30be025b7de8974ff791ed7473
Brahmantya Wardhana <bwardhana@hawk.iit.edu>
2023-11-06 15:00:46 -0600

    added the rest of the content of the page

--- js/script.js
+++ js/script.js
@@ -3,14 +3,3 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
-
-// function for username  digit requirement
-
-var form = document.getElementById('form');
-
-// alert for form completion
-function alertFunction() {
-  if (form.checkValidity()) {
-    alert("You have successfully signed up\nCheck your email for more information");
-  }
-}
